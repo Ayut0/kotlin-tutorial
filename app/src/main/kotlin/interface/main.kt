14dfc9230dd5b593a980f7f19fdfe7cc93732520
Yuto Yama <yuvto.0420@gmail.com>
2025-01-01 16:57:03 +0900

    Add interface

--- app/src/main/kotlin/interface/main.kt
+++ app/src/main/kotlin/interface/main.kt
@@ -1,1 +1,14 @@
-package interfaceDemo
+package interface
+
+interface Clickable{
+    fun click()
+}
+
+class Button:Clickable{
+    override fun click() = println("Button clicked")
+}
+
+fun main(){
+    val button = Button()
+    button.click()
+}
